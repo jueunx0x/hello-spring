0a47105877b3eecd965540052fa97132090ea694
sinjueun <jueunx0x@naver.com>
2024-10-10 21:22:42 +0900

    Second Commit_Spring Edu chap2

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,46 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 body부에 return 내용을 직접 넣어준다는 의미
+    public String helloString(@RequestParam("name")String name){
+        return "hello " + name; //"hello spring"으로 변경
+    }
+    //Json 형태로 호출
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //cmd+shift+enter 자동완성
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() { //cmd+n Generate
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
